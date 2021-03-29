1f391b20f1fffa0755923f852fbfc4363b71ca2d
iurimega13 <iurimega13@gmail.com>
2021-03-29 08:34:46 -0300

    Adionando objetos de teste

---
+++ TabelaDeClassificacao/script.js
@@ -0,0 +1,18 @@
+var paulo = {
+    nome: "paulo",
+    vitorias: 2,
+    empates: 5,
+    derrotas: 1,
+    pontos: 0
+}
+
+var rafa = {
+    nome: "rafa",
+    vitoris: 3,
+    empates: 5,
+    derrotas: 2,
+    pontos: 0
+}
+
+console.log(paulo);
+console.log(rafa);
