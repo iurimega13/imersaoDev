f18f402ba99ed00fea1f0ff18b77af5bb7a5cc23
iurimega13 <iurimega13@gmail.com>
2021-03-31 18:51:16 -0300

    Criando cartas

---
+++ superTrunfo/script.js
@@ -0,0 +1,18 @@
+var cartaPaulo = {
+    nome: "Seiya de pegaso",
+    ataque: 80,
+    defesa: 60,
+    magia: 90
+}
+var cartaRafa = {
+    nome: "Bulbasaur",
+    ataque: 70,
+    defesa: 65,
+    magia: 85
+}
+var cartaGui = {
+    nome: "Lord Darth Vader",
+    ataque: 88,
+    defesa: 62,
+    magia: 90
+}
