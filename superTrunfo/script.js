104017069a24fc3bc18f3e94effbd75b2c56b493
iurimega13 <iurimega13@gmail.com>
2021-03-31 20:49:13 -0300

    Modificando estrutura das cartas

--- superTrunfo/script.js
+++ superTrunfo/script.js
@@ -1,20 +1,27 @@
 var cartaPaulo = {
     nome: "Seiya de pegaso",
-    ataque: 80,
-    defesa: 60,
-    magia: 90
+    atributos: {
+        ataque: 80,
+        defesa: 60,
+        magia: 90
+    }
 }
 var cartaRafa = {
     nome: "Bulbasaur",
-    ataque: 70,
-    defesa: 65,
-    magia: 85
+    atributos: {
+        ataque: 70,
+        defesa: 65,
+        magia: 85
+    }
 }
 var cartaGui = {
     nome: "Lord Darth Vader",
-    ataque: 88,
-    defesa: 62,
-    magia: 90
+    atributos: {
+        ataque: 88,
+        defesa: 62,
+        magia: 90
+
+    }
 }
 var cartaMaquina
 var cartaJogador
@@ -24,7 +31,7 @@ function sortearCarta() {
     var numeroCartaMaquina = parseInt(Math.random() * 3)
     cartaMaquina = cartas[numeroCartaMaquina]
     console.log(cartaMaquina);
-    
+
     var numeroCartaJogador = parseInt(Math.random() * 3)
     while (numeroCartaJogador == numeroCartaMaquina) {
         var numeroCartaJogador = parseInt(Math.random() * 3)
