a4e9f61fd7d9770fbac9d92b93e6d9e9ea026380
iurimega13 <iurimega13@gmail.com>
2021-04-01 21:50:12 -0300

    Add cartas

--- superTrunfo/script.js
+++ superTrunfo/script.js
@@ -2,8 +2,8 @@ var cartaPaulo = {
     nome: "Seiya de pegaso",
     img: "http://2.bp.blogspot.com/-G16mpres2qY/UkI3ipzrRUI/AAAAAAAAE-I/7O9j4-xvOAQ/s1600/Seiyapegaso.jpg",
     atributos: {
-        ataque: 80,
-        defesa: 65,
+        ataque: 85,
+        defesa: 70,
         magia: 90
     }
 }
@@ -12,9 +12,9 @@ var cartaRafa = {
     nome: "Bulbasaur",
     img: "https://img.17qq.com/images/gwkwkqwwy.jpeg",
     atributos: {
-        ataque: 70,
-        defesa: 65,
-        magia: 85
+        ataque: 60,
+        defesa: 70,
+        magia: 80
     }
 }
 
@@ -22,14 +22,64 @@ var cartaGui = {
     nome: "Lord Darth Vader",
     img: "https://www.nerdsite.com.br/wp-content/uploads/2020/01/darth.jpg.webp",
     atributos: {
-        ataque: 88,
-        defesa: 65,
-        magia: 90
+        ataque: 93,
+        defesa: 80,
+        magia: 89
 
     }
 }
 
-var cartas = [cartaPaulo, cartaRafa, cartaGui]
+var cartaLol = {
+    nome: "Caitlyn",
+    imagem: "http://1.bp.blogspot.com/-K7CbqWc1-p0/VLc98v85s0I/AAAAAAAABqk/-ZB684VVHbg/s1600/Caitlyn_OriginalSkin.jpg",
+    atributos: {
+        ataque: 95,
+        defesa: 40,
+        magia: 10
+    }
+}
+
+var cartaNaruto = {
+    nome: "Naruto",
+    imagem: "https://conteudo.imguol.com.br/c/entretenimento/16/2017/06/27/naruto-1498593686428_v2_450x337.png",
+    atributos: {
+        ataque: 80,
+        defesa: 60,
+        magia: 100
+    }
+}
+
+var cartaHarry = {
+    nome: "Harry Potter",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/89ff10dd-aa41-4d17-ae8f-835281ebd3fd_49hp.jpg",
+    atributos: {
+        ataque: 70,
+        defesa: 50,
+        magia: 95
+    }
+}
+
+var cartaBatman = {
+    nome: "Batman",
+    imagem: "https://assets.b9.com.br/wp-content/uploads/2020/09/Batman-issue86-heder-1280x677.jpg",
+    atributos: {
+        ataque: 95,
+        defesa: 70,
+        magia: 0
+    }
+}
+
+var cartaMarvel = {
+    nome: "Capitã Marvel",
+    imagem: "https://cinepop.com.br/wp-content/uploads/2018/09/capitamarvel21.jpg",
+    atributos: {
+        ataque: 90,
+        defesa: 80,
+        magia: 0
+    }
+}
+
+var cartas = [cartaPaulo, cartaRafa, cartaGui, cartaLol, cartaNaruto, cartaHarry, cartaBatman, cartaMarvel]
 var cartaMaquina
 var cartaJogador
 
